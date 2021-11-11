a09994785a61199dc23a967168b92256a5f23e0f
Sami Horttanainen <horttsam@ad.helsinki.fi>
2021-11-11 17:03:53 +0200

    t5 step5

--- viikko2/branchit/index.py
+++ viikko2/branchit/index.py
@@ -1,8 +1,13 @@
 import logger from logger
+import summa import summa
+import erotus import erotus
 
 logger('aloitetaan')
 
 x = int(input('luku 1: '))
 y = int(input('luku2 :'))
+print(f'{summa(x, y)}')
+print(f'{erotus(x, y)}')
+
 
 logger('lopetetaa')
